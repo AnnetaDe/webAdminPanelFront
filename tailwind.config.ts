1af8914be1376a98077556a625ae45322320b911
AnnetaDe <anneta.liss@gmail.com>
2024-09-25 03:05:25 +0200

    front adminpanel

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,18 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import { COLORS } from './src/constants/color.constants';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      colors: COLORS,
+      backgroundImage: {
+        'primary-gradient': 'linear-gradient(to right, #ea9e4d, #FF825C)',
+        'secondary-gradient': 'linear-gradient(to right, #DA45AF, #FA5A87)',
       },
     },
   },
