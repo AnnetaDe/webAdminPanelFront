2932b679b27cf586434d6d05413c53fd7e34de3f
AnnetaDe <anneta.liss@gmail.com>
2024-10-06 21:01:04 +0200

    fix

--- src/constants/main.constants.ts
+++ src/constants/main.constants.ts
@@ -1,3 +1,3 @@
 export const API_URL = 'https://webadminpanelback.onrender.com/api';
-
+export const SERVER_URL = 'https://webadminpanelback.onrender.com';
 export const IS_CLIENT = typeof window !== 'undefined';
